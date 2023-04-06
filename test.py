0d1c9a355b9365bae65f9246ad299a1c69be075c
Salim <sabdelouahab@gmail.com>
2023-04-06 21:58:24 +0200

    Revert to standard repo statue

--- test.py
+++
@@ -1,9 +0,0 @@
-def greet(name):
-    print('Hello ' + name)
-
-people = ['Jack', 'Michelle', 'Christophe']
-
-for person in people:
-    greet(person)
-    
-print("We're going to do some machine learning")
